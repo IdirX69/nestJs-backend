763ece35fec89edd2da82af94bfd7d591357b227
IdirX69 <adil.idirr@gmail.com>
2024-03-05 14:25:46 +0100

    auth password&&email

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,24 @@
 import { Injectable } from '@nestjs/common';
-import { AuthBody } from '../../dist/auth/auth.controller';
+import { AuthBody } from '../auth/auth.controller';
 import { PrismaService } from 'src/users/prisma.service';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly prisma: PrismaService) {}
   async login({ authBody }: { authBody: AuthBody }) {
+    const { password, email } = authBody;
     const existingUser = await this.prisma.user.findUnique({
       where: {
-        email: authBody.email
+        email,
       },
-});
+    });
     if (!existingUser) {
       throw new Error("L'utilisateur n'existe pas");
-}
+    }
+    const isPasswordSame = password === existingUser.password;
+    if (!isPasswordSame) {
+      throw new Error('Le mot de passe est invalide');
+    }
     return existingUser;
   }
 }
